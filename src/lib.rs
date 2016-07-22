21cb8234d8e5715a01ffc8445b3331d76e25cdfe
rap2hpoutre <raphaelht@gmail.com>
2016-07-22 13:49:22 +0200

    thx so

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 extern crate hyper;
+extern crate rand;
 
 use std::io::Read;
 use hyper::client::Client;
+use rand::Rng;
 
 #[cfg(test)]
 mod tests {
@@ -13,10 +15,15 @@ mod tests {
 
 pub fn get_url_content(url: &str) -> Option<String> {
     let mut s = String::new();
-
     Client::new()
         .get(url)
         .send()
         .map(|mut res| res.read_to_string(&mut s).ok().map(|_| s))
         .unwrap_or(None)
+}
+
+pub fn rnd_take_n<T: Clone>(v: &mut [T], n: usize) -> Vec<T> {
+    rand::thread_rng().shuffle(v);
+    let r = &v[0 .. n];
+    r.to_vec()
 }
